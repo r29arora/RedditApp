8833937f976dc0a1412beb416639d9ceb0e2461b
Rajul Arora <rajul@wattpad.com>
2014-09-24 16:47:40 -0400

    Created new Swift project

---
+++ RedditApplication/RedditApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditApplication
+//
+//  Created by Rajul Arora on 2014-09-24.
+//  Copyright (c) 2014 Rajul Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
